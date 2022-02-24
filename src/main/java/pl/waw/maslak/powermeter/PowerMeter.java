3b08a2b4a041b5d2a1ccd7f2c329bc07170dd8c6
Piotr Maslak <piotr@maslak.waw.pl>
2022-02-24 13:50:24 +0100

    Next commit

--- src/main/java/pl/waw/maslak/powermeter/PowerMeter.java
+++ src/main/java/pl/waw/maslak/powermeter/PowerMeter.java
@@ -14,28 +14,53 @@ import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
 
 public class PowerMeter {
 
-    public static long timestamp1 = System.currentTimeMillis();
-    public static long timestamp2 = System.currentTimeMillis();
+    public static String host = "iot.maslak.waw.pl";
+    public static String client_id = "powermeter";
+    public static String topic_prefix = "";
+    public static String username = "maslak";
+    public static String password = "maslak";
+
+    public static long startTime = System.currentTimeMillis();
+    public static long stopTime = System.currentTimeMillis();
     public static String power = "0";
     public static double energy = 0;
-    public static MqttClient sampleClient;
-    //public static MqttMessage message;
+    public static MqttClient powerClient;
 
     public static void main(String[] args) throws InterruptedException {
 
-        String broker = "tcp://localhost:1883";
-        String clientId = "PowerMeter";
+        for (int i = 0; i < args.length; i++) {
+            String arg = args[i];
+            String value = args[++i];
+
+            if (arg.equalsIgnoreCase("-host")) {
+                host = value;
+            }
+            if (arg.equalsIgnoreCase("-client_id")) {
+                client_id = value;
+            }
+            if (arg.equalsIgnoreCase("-topic_prefix")) {
+                topic_prefix = value;
+            }
+            if (arg.equalsIgnoreCase("-username")) {
+                username = value;
+            }
+            if (arg.equalsIgnoreCase("-password")) {
+                password = value;
+            }
+        }
+
         MemoryPersistence persistence = new MemoryPersistence();
 
         try {
-            sampleClient = new MqttClient(broker, clientId, persistence);
+            powerClient = new MqttClient("tcp://" + host + ":1883", client_id, persistence);
             MqttConnectOptions connOpts = new MqttConnectOptions();
             connOpts.setCleanSession(true);
-            sampleClient.connect(connOpts);
-
-            //message.setQos(2);
-            sampleClient.publish("power", new MqttMessage(power.getBytes()));
-            sampleClient.publish("energy", new MqttMessage(String.format("%.4f", energy).getBytes()));
+            connOpts.setUserName(username);
+            connOpts.setPassword(password.toCharArray());
+            connOpts.setAutomaticReconnect(true);
+            powerClient.connect(connOpts);
+            powerClient.publish(topic_prefix + "power", new MqttMessage(power.getBytes()));
+            powerClient.publish(topic_prefix + "energy", new MqttMessage(String.format("%.4f", energy).getBytes()));
 
         } catch (MqttException ex) {
             Logger.getLogger(PowerMeter.class.getName()).log(Level.SEVERE, null, ex);
@@ -45,18 +70,17 @@ public class PowerMeter {
         final GpioController gpio = GpioFactory.getInstance();
         final GpioPinDigitalInput myButton1 = gpio.provisionDigitalInputPin(RaspiPin.GPIO_00, PinPullResistance.PULL_DOWN);
         myButton1.setShutdownOptions(true);
-
         myButton1.addListener(new GpioPinListenerDigital() {
             @Override
             public void handleGpioPinDigitalStateChangeEvent(GpioPinDigitalStateChangeEvent event) {
                 if (event.getState().isHigh()) {
-                    timestamp1 = timestamp2;
-                    timestamp2 = System.currentTimeMillis();
-                    power = String.valueOf(3600000 / (timestamp2 - timestamp1));
+                    startTime = stopTime;
+                    stopTime = System.currentTimeMillis();
+                    power = String.valueOf(3600000 / (stopTime - startTime));
                     energy = energy + 0.0001;
                     try {
-                        sampleClient.publish("power", new MqttMessage(power.getBytes()));
-                        sampleClient.publish("energy", new MqttMessage(String.format("%.4f", energy).getBytes()));
+                        powerClient.publish(topic_prefix + "power", new MqttMessage(power.getBytes()));
+                        powerClient.publish(topic_prefix + "energy", new MqttMessage(String.format("%.4f", energy).getBytes()));
                     } catch (MqttException ex) {
                         Logger.getLogger(PowerMeter.class.getName()).log(Level.SEVERE, null, ex);
                     }
@@ -68,5 +92,4 @@ public class PowerMeter {
             Thread.sleep(1000);
         }
     }
-
 }
